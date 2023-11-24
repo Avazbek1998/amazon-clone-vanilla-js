ec8a5e53aeb6b835c4d3d339271e1ac8e3cdc9e0
Avazbek1998 <citytashkent6@gmail.com>
2023-11-24 15:11:24 +0500

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -61,7 +61,6 @@ document.querySelectorAll(".js-add-to-cart")
   .forEach((button) => {
     button.addEventListener("click", () => {
       const productId = button.dataset.productId;
-
       let matchingItem;
 
       cart.forEach((item) => {
@@ -77,6 +76,14 @@ document.querySelectorAll(".js-add-to-cart")
           quantity: 1
         })
       }
+
+      let cartQuantity = 0;
+
+      cart.forEach(item => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
       console.log(cart);
     })
   })
