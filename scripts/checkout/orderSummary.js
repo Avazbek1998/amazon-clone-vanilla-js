a4931d559fdfeb8e0901a3b4b3e2ebec7aa0e1ae
Avazbek1998 <citytashkent6@gmail.com>
2023-11-26 18:00:43 +0500

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,11 +3,7 @@ import { products, getProduct } from "../../data/products.js"
 import { formatCurrency } from "../utils/money.js"
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js'
-
-const today = dayjs()
-const deliveryDate = today.add(7, 'days')
-deliveryDate.format('dddd, mmmm d')
-console.log(deliveryDate);
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 
 
@@ -127,6 +123,8 @@ export function renderOrderSummary() {
 
                 const container = document.querySelector(`.js-cart-item-container-${productId}`)
                 container.remove()
+
+                renderPaymentSummary()
             })
         })
 
@@ -136,6 +134,7 @@ export function renderOrderSummary() {
                 const { productId, deliveryOptionId } = element.dataset;
                 updateDeliveryOption(productId, deliveryOptionId)
                 renderOrderSummary()
+                renderPaymentSummary()
             })
         });
 }
